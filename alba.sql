a6720caa03a23bb3dfb075c6899c4be05399f45d
ABSVega <kuro_san@outlook.es>
2023-04-25 09:34:20 -0600

    first commit

---
+++ alba.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-04-2023 a las 17:04:01
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `alba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(2) NOT NULL,
+  `rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `rol`) VALUES
+(1, 'admin'),
+(2, 'secretaria'),
+(3, 'usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `rol_id` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `telefono`, `password`, `fecha`, `rol_id`) VALUES
+(1, 'waldo', 'fire.6215@gmail.com', '2381568261', '123', '2023-04-25 13:50:31', 1),
+(2, 'maria', 'maria@gmail.com', '2381562345', '123', '2023-04-19 13:51:17', 2),
+(3, 'milton', 'milton@gmail.com', '2381157680', '123', '2023-04-01 14:24:10', 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usurios_ibfk_1` (`rol_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usurios_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ login.php
@@ -0,0 +1,103 @@
+<?php
+include_once 'config/database.php';
+session_start();
+
+if (isset($_GET['cerrar_session'])) {
+    session_unset();
+
+    session_destroy();
+}
+
+if (isset($_SESSION['rol'])) {
+    switch ($_SESSION['rol']) {
+        case 1;
+            echo ("<script> window.location='CRUD/admin.php'; </script>");
+            break;
+
+        case 2;
+            header('location: secretaria.php');
+            break;
+
+        default:
+    }
+}
+
+if (isset($_POST['correo']) && isset($_POST['password'])) {
+    $correo = $_POST['correo'];
+    $password = $_POST['password'];
+
+    $db = new Database();
+    $query = $db->connect()->prepare('SELECT * FROM usuarios WHERE $correo = :correo AND password = :password LIMIT 1');
+    $query->execute(['correo' => $correo, 'password' => $password]);
+
+    $row = $query->fetch(PDO::FETCH_NUM);
+    if ($row == true) {
+        $rol = $row[3];
+        $_SESSION['rol'] = $rol;
+        switch ($_SESSION['rol']) {
+            case 1;
+                echo ("<script> window.location='CRUD/admin.php'; </script>");
+                break;
+
+            case 2;
+                header('location: secretaria.php');
+                break;
+
+            default:
+        }
+    } else {
+        echo "<div></div>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/loginstyle.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,600,0,0" />
+    <link rel="shortcut icon" href="img/ALBA_WEB_ELEMENTS-01.png" type="image/x-icon">
+    <title>Alba - Entrar o registrarse</title>
+</head>
+
+<body>
+
+    <div class="login-card-container">
+        <div class="login-card">
+            <div class="login-card-logo">
+                <img src="img/ALBA_WEB_ELEMENTS-01.png" alt="logo">
+            </div>
+            <div class="login-card-header">
+                <h1>Inicia Sesión</h1>
+                <div>recuerda colocar los datos correctos</div>
+            </div>
+            <form class="login-card-form" action="" method="POST">
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="text" placeholder="Correo" id="emailForm" name="correo" autofocus required>
+                </div>
+                <div class="form-item">
+                    <span class="form-item-icon material-symbols-rounded"></span>
+                    <input type="password" placeholder="Contraseña" id="passwordForm" name="password" required>
+                </div>
+                <div class="form-item-other">
+                    <div class="checkbox">
+                        <input type="checkbox" id="rememberMeCheckbox" checked>
+                        <label for="rememberMeCheckbox">Recordame</label>
+                    </div>
+                    <a href="#">Olvide mi contraseña</a>
+                </div>
+                <button type="submit">Iniciar Sesión</button>
+            </form>
+            <div class="login-card-footer">
+                ¿No tienes una cuenta? <a href="#">Registrate</a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
